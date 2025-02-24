kaspesk/kernel_motorola_sdm632

msm: Introduce simple kernel builder script

build.sh
@@ -1,0 +1,75 @@
+#!/bin/bash
+#
+# Compile script for Supra kernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Supra-channel-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$(pwd)/tc/clang-r498229"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+DEFCONFIG="channel_defconfig"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "AOSP clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b 17 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG
+	cp out/.config arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz
+
+kernel="out/arch/arm64/boot/Image.gz"
+
+if [ -f "$kernel" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+	elif ! git clone -q https://github.com/Bomb-Projects/AnyKernel3 -b master; then
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
+	fi
+	cp $kernel AnyKernel3
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout master &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi

crdroidandroid/android_kernel_motorola_sdm632

build.sh
@@ -1,0 +1,73 @@
+#!/bin/bash
+#
+# Compile script for Supra kernel
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Supra-channel-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$(pwd)/tc/clang-r522817"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+DEFCONFIG="channel_defconfig"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+	echo "AOSP clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b 18 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG
+	cp out/.config arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz
+
+kernel="out/arch/arm64/boot/Image.gz"
+
+if [ -f "$kernel" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+	elif ! git clone -q https://github.com/Bomb-Projects/AnyKernel3 -b channel; then
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
+	fi
+	cp $kernel AnyKernel3
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout master &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi